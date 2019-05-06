ca97bdfe1ac50dea4a8929572ed1f5e9f31587dd
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-05-06 15:55:19 +0300

    added the url structure of the project

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request
-from os import listdir
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>It works</h1>"
+    return render_template("index.html")
 
+@app.route("/search")
+def search():
+    pass
 
+@app.route("/results")
+def results():
+    pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
