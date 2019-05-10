ecc24eac446cdc644e1731bf91415f54cb7f52d2
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-05-10 18:06:33 +0300

    added favorites table with all the needed functions

--- database/database.py
+++ database/database.py
@@ -16,6 +16,11 @@ def create_database():
     directory TEXT NOT NULL)""")
     conn.commit()
 
+    db.execute("""CREATE TABLE IF NOT EXISTS favorites
+    (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    filename TEXT NOT NULL)""")
+    conn.commit()
+
 def directory_already_exists(directory):
     conn = sqlite3.connect(DATABASE)
     db = conn.cursor()
@@ -40,7 +45,6 @@ def add_directory_db(directory):
         conn.commit()
         return "Directory has successfully added in the database"
 
-
 def delete_directory_db(directory):
     conn = sqlite3.connect(DATABASE)
     db = conn.cursor()
@@ -53,6 +57,44 @@ def all_directories_db():
     db = conn.cursor()
 
     db.execute("SELECT directory FROM directories")
+    rows = [item[0] for item in db.fetchall()]
+    return rows
+
+def favorite_already_exists(filename):
+    conn = sqlite3.connect(DATABASE)
+    db = conn.cursor()
+
+    db.execute("SELECT filename FROM favorites WHERE filename = ?",(filename,))
+    exists = db.fetchone()
+
+    if(exists is not None):
+        return True
+    else:
+        return False
+
+def add_favorite_db(filename):
+    conn = sqlite3.connect(DATABASE)
+    db = conn.cursor()
+
+    if(favorite_already_exists(filename) == True):
+        return "Favorite is already in your list"
+    else:
+        db.execute("INSERT INTO favorites VALUES(?,?)",(None, filename))
+        conn.commit()
+        return "Filename has successfully add in your favorites"
+
+def delete_filename_db(filename):
+    conn = sqlite3.connect(DATABASE)
+    db = conn.cursor()
+
+    db.execute("DELETE FROM favorites WHERE filename = ?", (filename,))
+    conn.commit()
+
+def all_favorites_db():
+    conn = sqlite3.connect(DATABASE)
+    db = conn.cursor()
+
+    db.execute("SELECT filename FROM favorites")
     rows = [item[0] for item in db.fetchall()]
     return rows
 
